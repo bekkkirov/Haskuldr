581507c2ed56f71fc4e0c5e7ffaca900109de8ac
Vladyslav Bekirov <vbekirovv@gmail.com>
2025-07-12 18:47:46 +0300

    Added Error type.

---
+++ src/Haskuldr.Abstractions/Validation/ErrorType.cs
@@ -0,0 +1,11 @@
+﻿namespace Haskuldr.Abstractions.Validation;
+
+public enum ErrorType
+{
+    Validation = 0,
+    NotFound = 1,
+    Unauthorized = 2,
+    Forbidden = 3,
+    Conflict = 4,
+    Internal = 5,
+}
