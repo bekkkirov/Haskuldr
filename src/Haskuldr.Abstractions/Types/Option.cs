6835fd5945a45dfa9e3a30bed65c3d8e143aae35
Vladyslav Bekirov <vbekirovv@gmail.com>
2025-08-03 19:24:21 +0300

    Extended Option and Result classes.

--- src/Haskuldr.Abstractions/Types/Option.cs
+++ src/Haskuldr.Abstractions/Types/Option.cs
@@ -2,29 +2,62 @@
 
 namespace Haskuldr.Abstractions.Types;
 
-public readonly record struct Option<T>
+public readonly record struct Option<TValue> 
+    where TValue : notnull
 {
-    private readonly T? _value;
+    private readonly TValue? _value;
 
+    [MemberNotNullWhen(true, nameof(_value))]
     public bool HasValue { get; }
 
-    private Option(T value)
+    private Option(TValue value)
     {
         _value = value;
         
         HasValue = true;
     }
-
-    public static Option<T> FromValue(T value) => new(value);
     
-    public static Option<T> None() => default;
-    
-    public bool TryPickValue([NotNullWhen(true)] out T? value)
+    public bool TryPickValue([NotNullWhen(true)] out TValue? value)
     {
         value = HasValue ? _value : default;
         
         return HasValue;
     }
+
+    public TValue GetValue()
+    {
+        return HasValue 
+            ? _value 
+            : throw new InvalidOperationException("Cannot access value because option is empty");   
+    }
     
-    public static implicit operator Option<T>(T value) => FromValue(value);
+    public TValue? GetValueOrDefault()
+    {
+        return HasValue 
+            ? _value 
+            : default;
+    }
+    
+    public TResult Match<TResult>(Func<TValue, TResult> onValue, Func<TResult> onEmpty)
+    {
+        return HasValue ? onValue(_value) : onEmpty();
+    }
+    
+    public void Match(Action<TValue> onValue, Action onEmpty)
+    {
+        if (HasValue)
+        {
+            onValue(_value);
+        }
+        else
+        {
+            onEmpty();
+        }
+    }
+
+    public static Option<TValue> FromValue(TValue value) => new(value);
+    
+    public static Option<TValue> None => new ();
+    
+    public static implicit operator Option<TValue>(TValue value) => FromValue(value);
 }
--- src/Haskuldr.Abstractions/Types/Result.cs
+++ src/Haskuldr.Abstractions/Types/Result.cs
@@ -2,14 +2,19 @@
 
 namespace Haskuldr.Abstractions.Types;
 
-public readonly record struct Result<TValue, TError>
+public readonly record struct Result<TValue, TError> 
+    where TValue : notnull
+    where TError: notnull
 {
     private readonly TValue? _value;
-
     private readonly TError? _error;
 
+    [MemberNotNullWhen(true, nameof(_value))]
+    [MemberNotNullWhen(false, nameof(_error))]
     public bool IsSuccess { get; }
 
+    [MemberNotNullWhen(true, nameof(_error))]
+    [MemberNotNullWhen(false, nameof(_value))]
     public bool IsError => !IsSuccess;
 
     private Result(TValue value)
@@ -26,20 +31,67 @@ public readonly record struct Result<TValue, TError>
         IsSuccess = false;
     }
 
-    public bool TryPickValue([NotNullWhen(true)] out TValue? value)
+    public bool TryPickValue([NotNullWhen(true)] out TValue? value, out TError? error)
     {
         value = IsSuccess ? _value : default;
+        error = default;
 
         return IsSuccess;
     }
 
-    public bool TryPickError([NotNullWhen(true)] out TError? error)
+    public bool TryPickError([NotNullWhen(true)] out TError? error, out TValue? value)
     {
         error = IsError ? _error : default;
+        value = default;
 
         return IsError;
     }
 
+    public TValue GetValue()
+    {
+        return IsSuccess 
+            ? _value 
+            : throw new InvalidOperationException("Cannot access value because result is error");   
+    }
+    
+    public TValue? GetValueOrDefault()
+    {
+        return IsSuccess 
+            ? _value 
+            : default;
+    }
+
+    public TError GetError()
+    {
+        return IsError 
+            ? _error 
+            : throw new InvalidOperationException("Cannot access error because result is success");  
+    }
+
+    public TError? GetErrorOrDefault()
+    {
+        return IsError 
+            ? _error 
+            : default;
+    }
+
+    public TResult Match<TResult>(Func<TValue, TResult> onSuccess, Func<TError, TResult> onError)
+    {
+        return IsSuccess ? onSuccess(_value) : onError(_error!);
+    }
+    
+    public void Match(Action<TValue> onSuccess, Action<TError> onError)
+    {
+        if (IsSuccess)
+        {
+            onSuccess(_value);
+        }
+        else
+        {
+            onError(_error);
+        }
+    }
+    
     public static Result<TValue, TError> FromValue(TValue value)
     {
         return new Result<TValue, TError>(value);
