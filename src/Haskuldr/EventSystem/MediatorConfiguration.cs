b43a46c1833e4cf2066edc208825f84b83eca61b
Vladyslav Bekirov <vbekirovv@gmail.com>
2025-11-04 00:35:14 +0200

    Reworked mediator registration, added decorators.

---
+++ src/Haskuldr/EventSystem/MediatorConfiguration.cs
@@ -0,0 +1,66 @@
+﻿using System.Reflection;
+using Haskuldr.DependencyInjection;
+using Haskuldr.Mediator;
+using Haskuldr.Shared;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Haskuldr.EventSystem;
+
+public sealed class MediatorConfiguration
+{
+    private static readonly Type[] _baseHandlerTypes =
+    [
+        typeof(IRequestHandler<>),
+        typeof(IRequestHandler<,>)
+    ];
+
+    private readonly List<ServiceDescriptor> _handlers = [];
+    private readonly List<Type> _decorators = [];
+
+    public static IReadOnlyCollection<Type> BaseHandlerTypes => Array.AsReadOnly(_baseHandlerTypes);
+
+    public ServiceLifetime Lifetime { get; private set; } = ServiceLifetime.Transient;
+
+    public IReadOnlyCollection<ServiceDescriptor> Handlers => _handlers.AsReadOnly();
+
+    public IReadOnlyCollection<Type> Decorators => _decorators.AsReadOnly();
+
+    public MediatorConfiguration WithLifetime(ServiceLifetime lifetime)
+    {
+        Lifetime = lifetime;
+        
+        return this;
+    }
+    
+    public MediatorConfiguration WithHandlersFromAssembly(
+        Assembly assembly)
+    {
+        var serviceDescriptors = assembly
+                                 .GetGenericServiceDescriptors(_baseHandlerTypes, Lifetime)
+                                 .Where(x => x.ServiceType.GenericTypeArguments.All(a => !a.IsGenericParameter))
+                                 .ToList();
+
+        _handlers.AddRange(serviceDescriptors);
+
+        return this;
+    }
+
+    public MediatorConfiguration WithHandlersFromAssemblies(params Assembly[] assemblies)
+    {
+        ThrowHelper.ThrowIfEmpty(assemblies);
+        
+        foreach (var assembly in assemblies)
+        {
+            WithHandlersFromAssembly(assembly);
+        }
+
+        return this;
+    }
+
+    public MediatorConfiguration WithDecorator(Type decorator)
+    {
+        _decorators.Add(decorator);
+        
+        return this;
+    }
+}
--- src/Haskuldr/Mediator/DependencyInjectionExtensions.cs
+++ src/Haskuldr/Mediator/DependencyInjectionExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection;
-using Haskuldr.DependencyInjection;
-using Haskuldr.Shared;
+﻿using Haskuldr.EventSystem;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
@@ -10,24 +8,127 @@ public static class DependencyInjectionExtensions
 {
     public static IServiceCollection AddMediator(
         this IServiceCollection services,
-        ServiceLifetime lifeTime = ServiceLifetime.Transient,
-        params Assembly[] assemblies)
+        Action<MediatorConfiguration> configure)
     {
-        ThrowHelper.ThrowIfEmpty(assemblies);
+        var configuration = new MediatorConfiguration();
+        configure.Invoke(configuration);
         
-        services.AddScoped<IMediator, Mediator>();
-        
-        var baseHandlerTypes = new [] {typeof(IRequestHandler<>), typeof(IRequestHandler<,>)};
-        
-        foreach (var assembly in assemblies)
-        {
-            var serviceDescriptors = assembly.GetGenericServiceDescriptors(
-                baseHandlerTypes,
-                lifeTime);
+        services.AddMediator(configuration);
 
-            services.TryAddEnumerable(serviceDescriptors);
+        return services;
+    }
+
+    private static void AddMediator(
+        this IServiceCollection services,
+        MediatorConfiguration configuration)
+    {
+        services.Add(new ServiceDescriptor(typeof(IMediator), typeof(Mediator), configuration.Lifetime));
+        
+        services.TryAddEnumerable(configuration.Handlers);
+
+        foreach (var decorator in configuration.Decorators)
+        {
+            services.DecorateRequestHandler(decorator);
+        }
+    }
+    
+    private static IServiceCollection DecorateRequestHandler(
+        this IServiceCollection services,
+        Type decoratorType)
+    {
+        if (!decoratorType.IsGenericTypeDefinition)
+        {
+            throw new InvalidOperationException($"Decorator {decoratorType.FullName} is not an open generic type");
+        }
+
+        var requests = new List<Type> {
+            typeof(IRequestHandler<>),
+            typeof(IRequestHandler<,>)
+        };
+
+        var serviceType = decoratorType
+                          .GetInterfaces()
+                          .SingleOrDefault(x => 
+                              x.IsGenericType && 
+                              requests.Contains(x.GetGenericTypeDefinition()));
+
+        if (serviceType is null)
+        {
+            throw new InvalidOperationException($"Decorator {decoratorType.FullName} does not implement IRequestHandler interface");
+        }
+        
+        var descriptors = services
+                          .Where(s =>
+                              s.ServiceType.IsGenericType &&
+                              s.ServiceType.GetGenericTypeDefinition() == serviceType.GetGenericTypeDefinition())
+                          .ToList();
+
+        if (descriptors.Count == 0)
+        {
+            throw new InvalidOperationException(
+                $"No service types matching generic pattern {serviceType.FullName} are registered");
+        }
+
+        foreach (var descriptor in descriptors)
+        {
+            var newDecoratorType = decoratorType.MakeGenericType(descriptor.ServiceType.GenericTypeArguments);
+
+            services.DecorateInternal(serviceType, newDecoratorType);
         }
 
         return services;
+    }
+    
+    private static IServiceCollection DecorateInternal(
+        this IServiceCollection services,
+        Type serviceType,
+        Type decoratorType)
+    {
+        var descriptor = services.SingleOrDefault(s => s.ServiceType == serviceType);
+
+        if (descriptor is null)
+        {
+            throw new InvalidOperationException($"Service type {serviceType.FullName} is not registered");
+        }
+
+        services.Remove(descriptor);
+
+        services.Add(new ServiceDescriptor(
+            serviceType,
+            serviceProvider =>
+            {
+                var original = GetInstanceFromDescriptor(serviceProvider, descriptor);
+
+                return ActivatorUtilities.CreateInstance(serviceProvider, decoratorType, original);
+            },
+            descriptor.Lifetime));
+
+        return services;
+    }
+    
+    private static object GetInstanceFromDescriptor(
+        IServiceProvider serviceProvider,
+        ServiceDescriptor descriptor)
+    {
+        object instance;
+
+        if (descriptor.ImplementationType is not null)
+        {
+            instance = ActivatorUtilities.CreateInstance(serviceProvider, descriptor.ImplementationType);
+        }
+        else if (descriptor.ImplementationInstance is not null)
+        {
+            instance = descriptor.ImplementationInstance;
+        }
+        else if (descriptor.ImplementationFactory is not null)
+        {
+            instance = descriptor.ImplementationFactory(serviceProvider);
+        }
+        else
+        {
+            throw new InvalidOperationException($"Service type {descriptor.ImplementationType} is not registered");
+        }
+
+        return instance;
     }
 }
